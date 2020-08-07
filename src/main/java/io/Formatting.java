c394de636fbf4f57bb8a5c6232668e07b5458ee1
adamlagoda <adam_lagoda@interia.pl>
2020-08-07 14:01:37 +0200

    Blanks for formatting writers

--- src/main/java/io/Formatting.java
+++ src/main/java/io/Formatting.java
@@ -9,4 +9,12 @@ public class Formatting {
     public static void readFormattedFile() {
 
     }
+
+    public static void writeWithFormatter() {
+
+    }
+
+    public static void writeWithPrintWriter() {
+
+    }
 }
