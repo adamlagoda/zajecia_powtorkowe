c49af4a60df0c0c7b42bf3f8865bb4359ff30737
adamlagoda <adam_lagoda@interia.pl>
2020-08-07 14:00:43 +0200

    Solution to reading file

--- java-basics/src/main/java/io/Formatting.java
+++ java-basics/src/main/java/io/Formatting.java
@@ -1,13 +1,34 @@
 package io;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Formatting {
 
-    public static void main(String[] args) {
+    private static final String FILENAME = "src/main/resources/formatted.txt";
 
+    public static void main(String[] args) {
+        readFormattedFile();
     }
 
     public static void readFormattedFile() {
-
+        try (Scanner sc = new Scanner(new BufferedReader(new FileReader(FILENAME)))) {
+            sc.useLocale(Locale.US);
+            sc.useDelimiter("[\\t\\n\\r]+");
+            double firstLine = sc.nextDouble();
+            System.out.println("Double value: " + firstLine);
+            int secondLine = sc.nextInt();
+            System.out.println("Int value: " + secondLine);
+            long thirdLine = sc.nextLong();
+            System.out.println("Long value: " + thirdLine);
+            String fourthLine = sc.next();
+            System.out.println("String value: " + fourthLine);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void writeWithFormatter() {
