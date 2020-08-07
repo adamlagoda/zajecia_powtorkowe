4c9a50dcdf96666e8c358f8773d56e4f3b9b8ad3
adamlagoda <adam_lagoda@interia.pl>
2020-08-07 15:31:41 +0200

    Method to implement declaration

---
+++ src/main/java/io/CopyFile.java
@@ -0,0 +1,13 @@
+package io;
+
+import java.nio.file.Path;
+
+public class CopyFile {
+    public static void main(String[] args) {
+
+    }
+
+    public void copySourceToDestination(Path source, Path destination) {
+
+    }
+}
