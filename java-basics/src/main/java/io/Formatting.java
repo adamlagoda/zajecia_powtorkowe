09d838a7f2af4facf57bd7dcb0cebbf193df9e15
adamlagoda <adam_lagoda@interia.pl>
2020-08-07 15:26:36 +0200

    Solutions to formatting

--- java-basics/src/main/java/io/Formatting.java
+++ java-basics/src/main/java/io/Formatting.java
@@ -1,21 +1,21 @@
 package io;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.util.Formatter;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Formatting {
 
-    private static final String FILENAME = "src/main/resources/formatted.txt";
+    private static final String INPUT_FILENAME = "src/main/resources/formatted.txt";
+    private static final String OUTPUT_FILENAME = "src/main/resources/formatted-output.txt";
 
     public static void main(String[] args) {
-        readFormattedFile();
+        writeWithPrintWriter();
     }
 
     public static void readFormattedFile() {
-        try (Scanner sc = new Scanner(new BufferedReader(new FileReader(FILENAME)))) {
+        try (Scanner sc = new Scanner(new BufferedReader(new FileReader(INPUT_FILENAME)))) {
             sc.useLocale(Locale.US);
             sc.useDelimiter("[\\t\\n\\r]+");
             double firstLine = sc.nextDouble();
@@ -32,10 +32,38 @@ public class Formatting {
     }
 
     public static void writeWithFormatter() {
-
+        try (Scanner sc = new Scanner(new BufferedReader(new FileReader(INPUT_FILENAME)));
+             Formatter formatter = new Formatter(new BufferedWriter(new FileWriter(OUTPUT_FILENAME)))) {
+            sc.useLocale(Locale.US);
+            sc.useDelimiter("[\\t\\n\\r]+");
+            double firstLine = sc.nextDouble();
+            formatter.format("%.2f\n", firstLine);
+            int secondLine = sc.nextInt();
+            formatter.format("%d\n", secondLine);
+            long thirdLine = sc.nextLong();
+            formatter.format("%d\n", thirdLine);
+            String fourthLine = sc.next();
+            formatter.format("%s\n", fourthLine);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void writeWithPrintWriter() {
-
+        try (Scanner sc = new Scanner(new BufferedReader(new FileReader(INPUT_FILENAME)));
+             PrintWriter printWriter = new PrintWriter(new FileWriter(OUTPUT_FILENAME))) {
+            sc.useLocale(Locale.US);
+            sc.useDelimiter("[\\t\\n\\r]+");
+            double firstLine = sc.nextDouble();
+            printWriter.printf("%.2f\n", firstLine);
+            int secondLine = sc.nextInt();
+            printWriter.printf("%d\n", secondLine);
+            long thirdLine = sc.nextLong();
+            printWriter.printf("%d\n", thirdLine);
+            String fourthLine = sc.next();
+            printWriter.printf("%s\n", fourthLine);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
