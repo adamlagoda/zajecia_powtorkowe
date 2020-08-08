930bb3f404b184f21cdad3020de8731cd23a2bd4
adamlagoda <adam_lagoda@interia.pl>
2020-08-08 08:41:22 +0200

    Guarded lock main class

---
+++ java-basics/src/main/java/concurrency/guardedlock/GuardedLock.java
@@ -0,0 +1,9 @@
+package concurrency.guardedlock;
+
+public class GuardedLock {
+    public static void main(String[] args) {
+        Message message = new Message();
+        (new Thread(new Producer(message))).start();
+        (new Thread(new Consumer(message))).start();
+    }
+}
